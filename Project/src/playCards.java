c8dc4e6bde5ebb130467818965df7b94c455bc49
Connor <connor.chisholm96@gmail.com>
2015-03-29 13:51:15 -0300

    Updating Card class to instantiate card objects

--- Project/src/playCards.java
+++ Project/src/playCards.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class playCards//<--- just a test class to show common classes we all could use
 {
-   public static void main(String[] args)
-   {  
-      //from the Cards class:
-   
-      Cards.shuffle();    //<--- made for a deck with no Jokers - add if you wish of course
-      
-      Cards.deal(2,7);    //<--- = "2 players, 7 cards each."
-      
-      Cards.whatsMyHand();//<--- this shows the first hand (the user's) in Cards.allHands                          //     NB the output is not sorted, unfortunately. Any helpers?
-                         
-      Cards.exposeNext(); //<--- shows the next card flipped up from the deck.
-      
+	public static void main(String[] args)
+	{  
+		//from the Cards class:
 
-      //from the Card class:
-      
-      String[] card = Card.take();//<--- take from the deck. Deck reduces in size.
-      
-      if (card[0].equals(Cards.outPile.get(0)[0]) || card[1].equals(Cards.outPile.get(0)[1])) 
-      // = "if "card" has the same value or suit as the card on top of the outPile", then
-         Card.remit(card); //<--- card goes onto the outPile.(Crazy 8s rule) 
-   }
+		Card.shuffle();    //<--- made for a deck with no Jokers - add if you wish of course
+
+		Card.deal(2,7);    //<--- = "2 players, 7 cards each."
+
+		Card.whatsMyHand();//<--- this shows the first hand (the user's) in Cards.allHands                          //     NB the output is not sorted, unfortunately. Any helpers?
+
+		Card.exposeNext(); //<--- shows the next card flipped up from the deck.
+
+
+		//from the Card class:
+
+		String[] card = Card.take();//<--- take from the deck. Deck reduces in size.
+
+		if (card[0].equals(Card.outPile.get(0)[0]) || card[1].equals(Card.outPile.get(0)[1])) 
+			// = "if "card" has the same value or suit as the card on top of the outPile", then
+			Card.remit(card); //<--- card goes onto the outPile.(Crazy 8s rule) 
+	}
 }
