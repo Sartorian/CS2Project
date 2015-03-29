f5fa89f22599e799c575783d11f7b028b68706d9
Connor <connor.chisholm96@gmail.com>
2015-03-29 15:39:50 -0300

    Fixing

--- Project/src/Reference.java
+++ Project/src/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 	public static final String CLUB = "of Clubs";
 	public static final String HEART = "of Hearts";
 	public static final String DIAMOND = "of Diamonds";
-	public static final String FACE[] = {"Blank","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+	public static final String VALUE[] = {"Blank","A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 	//Face array is organized so we don't need to subtract from the index. Blank at FACE[0] will be default in the constructor.
 	
 	
