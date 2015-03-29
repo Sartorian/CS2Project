0f8c03b9fa6d57eb0d8197acc2eae66c3b4c38af
Connor <connor.chisholm96@gmail.com>
2015-03-29 13:30:04 -0300

    Added Reference Class

---
+++ Project/src/Reference.java
@@ -0,0 +1,9 @@
+
+public class Reference 
+{
+	public static final String SPADE = "of Spades";
+	public static final String CLUB = "of Clubs";
+	public static final String HEART = "of Hearts";
+	public static final String DIAMOND = "of Diamonds";
+	
+}
