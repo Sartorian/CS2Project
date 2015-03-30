ffc8291ba2a1971c6ce9aa1010f327aa27427f2f
Connor <connor.chisholm96@gmail.com>
2015-03-29 21:31:06 -0300

    Day is Over

---
+++ Project/src/UsedPile.java
@@ -0,0 +1,17 @@
+
+public class UsedPile
+{
+	private LinkedListForDeck cards;
+	
+	public UsedPile()
+	{
+		cards = new LinkedListForDeck();
+	}
+	
+	public LinkedListForDeck getCards()
+	{
+		return cards;
+	}
+	
+	
+}
